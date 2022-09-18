4ca787e383b1c330331ae04adf9239b6b31351fc
shentuzhigang <shentuzhigang01@weidian.com>
2022-09-18 14:34:07 +0800

    fix: ssr

--- packages/@starzkg/vuepress-plugin-live2d-widget/src/client/config.ts
+++ packages/@starzkg/vuepress-plugin-live2d-widget/src/client/config.ts
@@ -1,31 +1,35 @@
 import { defineClientConfig } from '@vuepress/client'
-import { L2Dwidget } from 'live2d-widget'
 import type { Live2dWidgetPluginOptions } from '../shared/index.js'
 import { changeDialogMessage } from './utils/index.js'
+
+declare const __VUEPRESS_SSR__: boolean
 
 declare const LIVE2D_WIDGET_OPTIONS: Live2dWidgetPluginOptions
 
 const live2dWidgetPluginOptions = LIVE2D_WIDGET_OPTIONS
 
 export default defineClientConfig({
-  enhance() {
-    if (live2dWidgetPluginOptions.dev?.log) {
-      L2Dwidget.on('*', (name) => {
-        console.log(
-          '%c EVENT ' + '%c -> ' + name,
-          'background: #222; color: yellow',
-          'background: #fff; color: #000'
-        )
-      })
+  async enhance() {
+    if (!__VUEPRESS_SSR__) {
+      const { L2Dwidget } = await import('live2d-widget')
+      if (live2dWidgetPluginOptions.dev?.log) {
+        L2Dwidget.on('*', (name) => {
+          console.log(
+            '%c EVENT ' + '%c -> ' + name,
+            'background: #222; color: yellow',
+            'background: #fff; color: #000'
+          )
+        })
+      }
+      if (live2dWidgetPluginOptions.dialog?.script) {
+        L2Dwidget.on('tapface', (name) => {
+          changeDialogMessage('人家已经不是小孩子了')
+        })
+        L2Dwidget.on('tapbody', (name) => {
+          changeDialogMessage('哎呀！别碰我！')
+        })
+      }
+      L2Dwidget.init(live2dWidgetPluginOptions)
     }
-    if (live2dWidgetPluginOptions.dialog?.script) {
-      L2Dwidget.on('tapface', (name) => {
-        changeDialogMessage('人家已经不是小孩子了')
-      })
-      L2Dwidget.on('tapbody', (name) => {
-        changeDialogMessage('哎呀！别碰我！')
-      })
-    }
-    L2Dwidget.init(live2dWidgetPluginOptions)
   },
 })
