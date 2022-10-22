68a77b0b69d445702a6f70371ac73dcbe818def6
shentuzhigang <shentuzhigang01@weidian.com>
2022-10-22 19:32:40 +0800

    fix: update vuepress

--- docs/.vuepress/configs/meta.ts
+++
@@ -1,8 +0,0 @@
-import { createRequire } from 'node:module'
-import { fs } from '@vuepress/utils'
-
-const require = createRequire(import.meta.url)
-
-export const version = fs.readJSONSync(
-  require.resolve('@starzkg/vuepress-plugin-analytics/package.json')
-).version
